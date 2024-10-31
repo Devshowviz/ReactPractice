cf44dd7bbf4e184c77608f94d00fe47e89f407b6
HMAKARS <devshowviz@gmail.com>
2024-10-31 15:41:13 +0900

    first commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import App from "./App";
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import Clock from "./chapter_04/Clock";
+
+setInterval(() => {
+    ReactDOM.render(
+        <React.StrictMode>
+            <Clock/>
+        </React.StrictMode>,
+        document.getElementById('root')
+    );
+},1000);
+/*
 root.render(
   <React.StrictMode>
-    <App />
+      <Clock/>
   </React.StrictMode>
 );
+ */
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
